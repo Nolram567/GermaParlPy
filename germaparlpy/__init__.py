9c35bdf1d7f641cce89aeaa575a7b6fc514f50dc
Nolram567 <74004417+Nolram567@users.noreply.github.com>
2026-05-21 12:25:27 +0200

    chore: bump version to 1.1.0

--- germaparlpy/__init__.py
+++ germaparlpy/__init__.py
@@ -5,7 +5,7 @@ The GermaParlPy Python package provides functionality to deserialize, serialize,
 
 import logging
 
-__version__ = "1.0.6"
+__version__ = "1.1.0"
 
 logger = logging.getLogger("germaparlpy")
 logger.addHandler(logging.NullHandler())
